54f33ef6b503cca0cb0a93ca504fb9dde1bbb154
Szymon <s.mazurek@sanoscience.org>
2023-07-25 11:02:50 +0200

    Added AttentionBlock

--- networks/att_squeeze_unet_torch.py
+++ networks/att_squeeze_unet_torch.py
@@ -23,68 +23,59 @@ from tensorflow.keras.activations import sigmoid
 from tensorflow.keras import Model, Sequential
 
 
-# class AttSqueezeUNetTorch(nn.Module):
-class FireModuleTF(tf.keras.Model):
-    def __init__(self, fire_id, squeeze, expand):
-        super(FireModuleTF, self).__init__(name="")
-
-        self.fire = Sequential()
-        self.fire.add(
-            Conv2D(
-                squeeze,
-                (1, 1),
-                activation="relu",
-                padding="same",
-                kernel_initializer="he_normal",
-            )
-        )
-        self.fire.add(BatchNormalization(axis=-1))
-        self.left = Conv2D(
-            expand,
-            (1, 1),
-            activation="relu",
-            padding="same",
-            kernel_initializer="he_normal",
-        )
-        self.right = Conv2D(
-            expand,
-            (3, 3),
-            activation="relu",
-            padding="same",
-            kernel_initializer="he_normal",
-        )
-
-    def call(self, x):
-        x = self.fire(x)
-        print(f"tf fire {x.shape}")
-        left = self.left(x)
-        print(f"tf left {left.shape}")
-        right = self.right(x)
-        print(f"tf right {right.shape}")
-        x = tf.concat([left, right], axis=-1)
-        print(f"tf out {x.shape}")
-        return x
-
-
 class FireModule(nn.Module):
-    def __init__(self, fire_id, squeeze, expand):
+    def __init__(self, fire_id, squeeze, expand, in_channels=3):
         super(FireModule, self).__init__()
 
         self.fire = nn.Sequential(
-            nn.Conv2d(3, squeeze, kernel_size=1, padding="same"),
+            nn.Conv2d(in_channels, squeeze, kernel_size=1, padding="same"),
             nn.BatchNorm2d(squeeze),
             nn.ReLU(inplace=True),
         )
-        self.left = nn.Conv2d(squeeze, expand, kernel_size=1, padding="same")
-        self.right = nn.Conv2d(squeeze, expand, kernel_size=3, padding="same")
+        self.left = nn.Sequential(
+            nn.Conv2d(squeeze, expand, kernel_size=1, padding="same"),
+            nn.ReLU(inplace=True),
+        )
+        self.right = nn.Sequential(
+            nn.Conv2d(squeeze, expand, kernel_size=3, padding="same"),
+            nn.ReLU(inplace=True),
+        )
 
     def forward(self, x):
         x = self.fire(x)
-        print(f"torch fire {x.shape}")
         left = self.left(x)
-        print(f"torch left {left.shape}")
         right = self.right(x)
-        print(f"torch right {right.shape}")
         x = torch.cat([left, right], dim=1)
-        print(f"torch out {x.shape}")
         return x
+
+
+class AttentionBlock(nn.Module):
+    def __init__(self, F_g, F_l, F_int):
+        super(AttentionBlock, self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(
+                F_g, F_int, kernel_size=1, stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm2d(F_int),
+        )
+        self.W_x = nn.Sequential(
+            nn.Conv2d(
+                F_l, F_int, kernel_size=1, stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm2d(F_int),
+        )
+        self.psi = nn.Sequential(
+            nn.Conv2d(
+                F_int, 1, kernel_size=1, stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid(),
+        )
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
